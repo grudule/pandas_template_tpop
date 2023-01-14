f9057370386104ad170ab1ff1c589620f183722f
Alexandre <alsag3@ulaval.ca>
2023-01-14 17:22:14 -0500

    adding plotting method

--- test/template_data_extractor_test.py
+++ test/template_data_extractor_test.py
@@ -1,7 +1,13 @@
-import pandas as pd 
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 # get your data with the correct function pd.read_format_file("absolute/path/to/your/file", sep= "<separator used in file>)
 # note tabular file sep = "\t"
 data = pd.read_csv("test\Values.csv", sep=",")
 # print your data cause why not, use head() for begin only
 print(data)
+
+# in case of a multiple column csv change the name of your x variable and add in the list the data you want to draw in function of x: 
+data.plot(x="Distance_(pixels)",y=["Gray_Value"])
+plt.show()
