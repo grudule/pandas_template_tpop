ba36dec9813de6c053cbbf010afec0bbc22d3c69
Alexandre <alsag3@ulaval.ca>
2023-01-14 19:31:03 -0500

    minor fix

--- src/template_data_extractor_test.py
+++ src/template_data_extractor_test.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 
 # pandas offical documentation : https://pandas.pydata.org/docs/
+
 # get your data with the correct function :
 # pd.read_format_file("absolute/path/to/your/file", 
 #                      sep= "<separator used in file>
